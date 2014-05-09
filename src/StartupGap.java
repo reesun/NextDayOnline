4fae096b3b2b3cc1710e43610504a4eb76fda2e6
Alex <reesun.huang@gmail.com>
2014-05-09 11:11:51 +0800

    启动间隔

--- src/StartupGap.java
+++ src/StartupGap.java
@@ -1,9 +1,12 @@
-import java.nio.channels.SelectableChannel;
 import java.sql.Connection;
 import java.util.List;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 
+import beans.DateSK;
+import beans.DeviceInfo;
 import beans.ProductID;
 
 import utils.DBHandler;
@@ -35,7 +38,7 @@ public class StartupGap {
 			int prodcutid = productID.getId();
 			// 取前30天的启动记录
 			long s = System.currentTimeMillis();
-//			ETL.restoreStartupClientdata(dwConn, run, fromDateSK, endDateSK-1, prodcutid);
+			ETL.restoreStartupClientdata(dwConn, run, fromDateSK, endDateSK-1, prodcutid);
 			long e = System.currentTimeMillis();
 			System.out.println("Time: " + (e - s) / 1000);
 			
@@ -47,35 +50,73 @@ public class StartupGap {
 			System.out.println(hour.toString());
 			
 			// 30天内，只有一次启动
-			sql = "select count(1) from (SELECT `deviceidentifier`,count(1) num FROM `razor_tmp_startupgap_1` group by `deviceidentifier`having num = 1) tt";
+			sql = "select count(1) from " +
+				" (SELECT `deviceidentifier`,count(1) num " +
+				"	FROM `razor_tmp_startupgap_1` " +
+				"	group by `deviceidentifier`having num = 1) tt";
+			
 			Object oneStart = run.query(dwConn, sql, onHandler);
 			System.out.println(oneStart.toString());
 			
-			Object day1, day2, day3, day4, day5, day6, day7, day14, day30;
-			sql = "SELECT DISTINCT  `deviceidentifier`  FROM  `razor_tmp_startupgap_1` ";
+			int day1 = 0, day2 =0, day3=0, day4=0, day5=0, day6=0, day7=0, day14=0, day30=0;
+			sql = "SELECT `deviceidentifier` uid FROM `razor_tmp_startupgap_1` " +
+				" group by `deviceidentifier` having count(1) > 1 ";
 			
+			ResultSetHandler<List<DeviceInfo>> deviceInfoHandler = 
+						new BeanListHandler<DeviceInfo>(DeviceInfo.class);
 			
-			// +1天
+			ResultSetHandler<List<DateSK>> dataskHandler = 
+					new BeanListHandler<DateSK>(DateSK.class);
 			
-			// +2
+			String deviceQuery = "SELECT date_sk FROM  `razor_tmp_startupgap_1` " +
+					" WHERE  `deviceidentifier` =  ?";
 			
-			// +3
+			List<DeviceInfo> devices = run.query(dwConn, sql, deviceInfoHandler);
+			System.out.println(devices.size());
+			for(DeviceInfo device: devices){
+				List<DateSK> datesks = run.query(dwConn, deviceQuery, dataskHandler, device.getUid());
+				
+				for(int index = 0; index < datesks.size()-1; index++){
+					int from = datesks.get(index).getDate_sk();
+					int next = datesks.get(index+1).getDate_sk();
+					int startgap = next - from;
+					switch (startgap) {
+					case 1:
+						day1 += 1;
+						break;
+					case 2:
+						day2 += 1;
+						break;
+					case 3:
+						day3 += 1;
+						break;
+					case 4:
+						day4 += 1;
+						break;
+					case 5:
+						day5 += 1;
+						break;
+					case 6:
+						day6 += 1;
+						break;
+					case 7:
+						day7 += 1;
+						break;
+					case 14:
+						day14 += 1;
+						break;
+					case 30:
+						day30 += 1;
+						break;
+
+					}
+				}
+			}
 			
-			// +4
-			
-			// +5
-			
-			// +6
-			
-			// +7
-			
-			// +14 
-			
-			// +30 
-			
-			
-			sql = "insert into  razor_sum_startup_gap(endTime, onstart, hour, product_id) values(?,?,?,?)";
-//			run.update(dwConn, sql, dateString, oneStart, hour, prodcutid);
+			sql = "insert into  razor_sum_startup_gap(endTime, onstart, hour, 1day, 2day, 3day, 4day, 5day, 6day, 7day, 14day, 30day, product_id)" +
+					" values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			run.update(dwConn, sql, dateString, oneStart.toString(), hour.toString(), 
+					day1, day2, day3, day4, day5, day6, day7, day14, day30, prodcutid);
 			
 			e = System.currentTimeMillis();
 			System.out.println("aa time:"+ (e - s) / 1000);
@@ -86,3 +127,4 @@ public class StartupGap {
 	
 
 }
+
---
+++ src/beans/DateSK.java
@@ -0,0 +1,15 @@
+package beans;
+
+import java.io.Serializable;
+
+public class DateSK implements Serializable {
+	private int date_sk;
+
+	public int getDate_sk() {
+		return date_sk;
+	}
+
+	public void setDate_sk(int date_sk) {
+		this.date_sk = date_sk;
+	}
+}
